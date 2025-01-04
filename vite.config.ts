94e57b6df9858ba00c620c86fbf1d4411106f227
vivek.gautam5 <vivek.gautam5@flipkart.com>
2025-01-04 22:56:24 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/teleparty-socket/', // Replace <your-repo-name> with the name of your GitHub repository
 })
